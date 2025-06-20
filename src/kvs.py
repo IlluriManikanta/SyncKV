1a844b7ce381238b156d8094204e3c6e43187af3
maniilluri <manikanta.illuri2013@gmail.com>
2025-06-20 12:56:35 -0700

    added vector clock and nodeID(set to env variable later)

--- src/kvs.py
+++ src/kvs.py
@@ -1,18 +1,31 @@
 from flask import jsonify
+from vector_clocks import VectorClock
+
+NODE_ID = "A"  # later this could be set via env variable
 
 kv_store = {}
+vc = VectorClock(NODE_ID)
+
 
 def store(key, value, metadata):
-    kv_store[key] = value
-    return jsonify({"message": "Stored", "causal-metadata": {}}), 200
+    vc.increment()  # log a new event by this node
+    current_clock = vc.get()  # get the updated clock state
+    kv_store[key] = (value, current_clock)
+
+    return jsonify({"message": "Stored", "causal-metadata": current_clock}), 200
+
 
 def retrieve(key, metadata):
     if key in kv_store:
-        return jsonify({"val": kv_store[key], "causal-metadata": {}}), 200
+        value, stored_clock = kv_store[key]
+        return jsonify({"val": value, "causal-metadata": stored_clock}), 200
+
     return jsonify({"error": "Key not found"}), 404
 
 def delete(key, metadata):
     if key in kv_store:
+        vc.increment()
         del kv_store[key]
-        return jsonify({"message": "Deleted", "causal-metadata": {}}), 200
+        return jsonify({"message": "Deleted", "causal-metadata": vc.get()}), 200
+
     return jsonify({"error": "Key not found"}), 404
